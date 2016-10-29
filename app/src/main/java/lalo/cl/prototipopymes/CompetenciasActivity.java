6403b4e96f51a525deda0315432e75699b52ded8
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-10-29 19:18:54 -0300

    fix color

--- app/src/main/java/lalo/cl/prototipopymes/CompetenciasActivity.java
+++ app/src/main/java/lalo/cl/prototipopymes/CompetenciasActivity.java
@@ -23,15 +23,16 @@ public class CompetenciasActivity extends AppCompatActivity {
 
     private void fill() {
         ArrayList<Producto> productos = new ArrayList<>();
+        ArrayList<Producto> empty = new ArrayList<>();
         productos.add(new Producto("kg Pan", 600, 700));
         productos.add(new Producto("kg Harina", 500, 500));
         productos.add(new Producto("1.5 lt Bebida", 1000, 900));
         adapter.add(new Competencia("Lider", "Supermercado", "2 km", R.drawable.lider, productos));
 
-        adapter.add(new Competencia("Jumbo", "Supermercado", "3 km", R.drawable.jumbo, null));
+        adapter.add(new Competencia("Jumbo", "Supermercado", "3 km", R.drawable.jumbo, empty));
 
-        adapter.add(new Competencia("El Ocaso", "Panaderia", "500 m", R.drawable.panaderia, null));
-        adapter.add(new Competencia("Primavera", "Minimarket", "50 m", R.drawable.mini, null));
-        adapter.add(new Competencia("Don Juan", "Minimarket", "600 m", R.drawable.mini2, null));
+        adapter.add(new Competencia("El Ocaso", "Panaderia", "500 m", R.drawable.panaderia, empty));
+        adapter.add(new Competencia("Primavera", "Minimarket", "50 m", R.drawable.mini, empty));
+        adapter.add(new Competencia("Don Juan", "Minimarket", "600 m", R.drawable.mini2, empty));
     }
 }
