403825135d4a05e0bf0800a4bb57cff8de52da16
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-11-09 18:49:01 -0300

    zd

--- app/src/main/java/lalo/cl/prototipopymes/InternosActivity.java
+++ app/src/main/java/lalo/cl/prototipopymes/InternosActivity.java
@@ -1,5 +1,6 @@
 package lalo.cl.prototipopymes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,7 +14,7 @@ public class InternosActivity extends AppCompatActivity {
     }
 
     public void goToVentasTotales(View v) {
-
+        startActivity(new Intent(this, VentasActivity.class));
     }
 
     public void goToVentasPorLineaDeProductos(View v) {
